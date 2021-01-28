5c5e6c5afe812c9e334fc5f0506edbf5478d9ed0
bs <brianbs3@gmail.com>
2021-01-27 21:48:44 -0500

    working on weather return from memcache

--- api/routes/weather.js
+++ api/routes/weather.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const router = require('express').Router();
+const Memcached = require('memcached');
 
 router.get('/', (req, res) => {
-    return res.json({message: 'temperature endpoint'});
+  let memcached = new Memcached('localhost:11211', {retries:10,retry:10000,remove:true});
+  let weather = memcached.get('shopWeather1_weather', function(err, data){
+    if(!err){
+      return res.json(data);
+    }
+    else{
+      return res.json({message: 'we got here, so there was an error getting from memcache...'});
+    }
+  });
 });
 
 module.exports = router;
