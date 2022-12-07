a2d5a4dad1e8c52225bb2bb503c6b4d1eb47da91
bs <brianbs3@gmail.com>
2022-12-07 03:52:10 +0000

    bringing on shopWeather2

--- api/utils/db.js
+++ api/utils/db.js
@@ -13,8 +13,11 @@ const getRedisWeather = async () => {
     .get('F1')
     .get('F2')
     .get('CPU')
+    .get('timestamp')
+    .get('hostname')
+    .get('CPU')
     .exec((err, replies) => {
-        resolve({F1: replies[0],F2: replies[1], CPU: replies[2]});
+        resolve({F1: replies[0],F2: replies[1], CPU: replies[2], TIMESTAMP: replies[3], hostname: replies[4], CPU: replies[5]});
     });
   }
   catch(error){
--- get_temp.py
+++ get_temp.py
@@ -1,17 +1,29 @@
-import os, glob, time, json, redis
+import os, glob, time, json, redis, rrdtool
+from datetime import datetime, timedelta
+
+timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000Z')
+print(timestamp)
 
 # 28-0114551eafaa has black mark
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-r = redis.Redis(
+red = redis.Redis(
     host='localhost')
+
+red.set('hostname', os.uname()[1])
+red.set('timestamp', timestamp)
 
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 temps = {}
+
+def get_cpu_temp():
+  f = open('/sys/class/thermal/thermal_zone0/temp')
+  lines = f.readlines()
+  return (float(lines[0]) / 1000.0) * 9.0 / 5.0 + 32.0
 
 def read_temp_raw(device):
     f = open(device + "/w1_slave", 'r')
@@ -32,11 +44,32 @@ def read_temp(device, d):
         return json.dumps({"device": d, "c": temp_c, "f": temp_f})
   
 devices = ["28-011455020eaa", "28-0114551eafaa"]
+rrd = {}
+
+
+
 for d in devices:
     dev = base_dir + d
     temps = read_temp(base_dir + d, d)
     #r.set(d, json.dumps(temps)['f'])
     str =   json.loads(temps)
-    print(str['f'])
-    r.set(d, str['f'])
+    if(d == '28-0114551eafaa'):
+      rrd.update({'F1': str['f']})
+      rrd.update({'C1': str['c']})
+      red.set('F1', str['f'])
+      red.set('C1', str['c'])
+    else:
+      rrd.update({'Inside': str['f']})
+      rrd.update({'F2': str['f']})
+      rrd.update({'C2': str['c']})
+      red.set('F2', str['f'])
+      red.set('C2', str['c'])
+
+
+humidity = 0
+cpuTemp = get_cpu_temp()
+red.set('CPU', cpuTemp)
+rrdString = "N:{}:{}:{}:{}:{}:{}:{}".format(rrd['F1'], rrd['C1'], rrd['F2'], rrd['C2'], humidity, 0, cpuTemp)
+rrdtool.update('/data/weather/shopWeather2.rrd', rrdString)
+print(rrdString)
 
