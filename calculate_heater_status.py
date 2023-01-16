3961a2f4d4a785ea6330111eec359bf14001a5fd
bs <brianbs3@gmail.com>
2023-01-15 19:47:50 -0500

    convert calculate to use redis for target temp

--- calculate_heater_status.py
+++ calculate_heater_status.py
@@ -9,6 +9,7 @@ heater_status = 'off'
 if __name__ == '__main__':
     try:
 
+      target_temp = float(red.get('shop_heater_target_temp'))
       f1 = float(red.get('inside_temp_f'))
       if f1 < target_temp:
         heater_status = 'on'
@@ -16,6 +17,7 @@ if __name__ == '__main__':
       red.set('heater_status', heater_status)
       print("current temp: {}".format(f1))
       print("heater status: {}".format(heater_status))
+      print("target temp: {}".format(target_temp))
     except KeyboardInterrupt:
         print("killed by keystroke")
 
