74e81bb2503eae694b5c69a386363152174f2b27
bsizemore <bsizemore@apple.com>
2023-10-31 11:47:20 -0400

    change up pico table

--- api/static/js/bsWeather.js
+++ api/static/js/bsWeather.js
@@ -10,6 +10,24 @@ function c2f(c) {
 }
 
 function populateDeviceTable(){
+    const visibleCols = [
+        'ID',
+        'HOSTNAME',
+        'MAC',
+        'LOCATION',
+        'POLL_INTERVAL',
+        'VERSION',
+        'IP',
+        'UPDATED',
+        'HUMIDITY',
+        'ONEWIRE_TEMP_C',
+        'HUMIDITY_TEMP_C',
+        'ONBOARD_TEMP_C',
+        'INSTALLED',
+        'HASCASE',
+        'SUBLOCATION',
+        'NOTES'
+    ]
     $.ajax({
         type: 'GET',
         contentType: 'application/json',
@@ -20,33 +38,37 @@ function populateDeviceTable(){
             let thead = $(`<thead>`);
             let tbody = $(`<tbody>`);
             Object.keys(data[0]).forEach((header) => {
-                thead.append(`<th>${header.toUpperCase()}</th>`);
+                if(visibleCols.includes(header.toUpperCase())){
+                    thead.append(`<th>${header.toUpperCase()}</th>`);
+                }
             });
             
             Object.keys(data).forEach((col) => {
                 let tr = $(`<tr>`)
                 Object.keys(data[col]).forEach((val) => {
-                    if(val === "updated"){
-                        let updatedClass = "btn btn-success";
-                        const ts = moment(data[col][val]);
-                        if(ts < moment().subtract(5, 'minutes'))
-                            updatedClass = "btn btn-danger";
-                        tr.append(`<td><button type=button class='${updatedClass}'>${ts.format('DD-MMM-YYYY hh:mm:ss A')}</button></td>`)
-                    }
-                    else if(val === "version") {
-                        let versionClass = "btn btn-success"
-                        if(data[col][val] !== latestVersion)
-                            versionClass = "btn btn-danger"
-                        tr.append(`<td><button type=button class='${versionClass}'>${data[col][val]}</button></td>`)
-                    }
-                    else if(val === "onewire_temp_c"){
-                        tr.append(`<td>${c2f(data[col][val])} &#176;F (${data[col][val]} &#176;C)</td>`)
-                    }
-                    else if(val ==="ledColor"){
-                        tr.append(`<td><div class='led led_${data[col][val]}'>LED</div></td>`)
-                    }
-                    else{
-                        tr.append(`<td>${data[col][val]}</td>`)
+                    if(visibleCols.includes(val.toUpperCase())){
+                        if(val === "updated"){
+                            let updatedClass = "btn btn-success";
+                            const ts = moment(data[col][val]);
+                            if(ts < moment().subtract(5, 'minutes'))
+                                updatedClass = "btn btn-danger";
+                            tr.append(`<td><button type=button class='${updatedClass}'>${ts.format('DD-MMM-YYYY hh:mm:ss A')}</button></td>`)
+                        }
+                        else if(val === "version") {
+                            let versionClass = "btn btn-success"
+                            if(data[col][val] !== latestVersion)
+                                versionClass = "btn btn-danger"
+                            tr.append(`<td><button type=button class='${versionClass}'>${data[col][val]}</button></td>`)
+                        }
+                        else if(val === "onewire_temp_c"){
+                            tr.append(`<td>${parseFloat(c2f(data[col][val])).toFixed(3)} &#176;F (${parseFloat(data[col][val]).toFixed(3)} &#176;C)</td>`)
+                        }
+                        else if(val === "ledColor"){
+                            tr.append(`<td><div class='led led_${data[col][val]}'>LED</div></td>`)
+                        }
+                        else{
+                            tr.append(`<td>${data[col][val]}</td>`)
+                        }
                     }
                     
                     tbody.append(tr);
